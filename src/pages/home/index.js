50aca08569a26d608e77be16ae75b95e86844977
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-08 11:05:32 -0300

    Input de pesquisa de receita

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,75 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import { Logo } from "../../components/logo";
 
 export function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSearch() {
+    console.log("Você digitou:", inputValue);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Página Home!!!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite o nome da comida..."
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+        />
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#f00",
+    flex: 1,
+    backgroundColor: "#f3f9ff",
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#fff",
+    width: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "90%",
+    maxWidth: "90%",
+    height: 54,
   },
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,51 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Home } from "../pages/home";
-import { Search } from "../pages/search";
 import { Favorites } from "../pages/favorites";
+
+import { Ionicons } from "@expo/vector-icons";
 
 const tab = createBottomTabNavigator();
 
 export function Routes() {
   return (
-    <tab.Navigator>
-      <tab.Screen name="Home" component={Home} />
-      <tab.Screen name="Search" component={Search} />
-      <tab.Screen name="Favorites" component={Favorites} />
+    <tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#121212",
+
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 0,
+        },
+      }}
+    >
+      <tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="home" size={size} color="#000" />;
+            }
+            return <Ionicons name="home-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="heart" size={size} color="#ff4141" />;
+            }
+            return <Ionicons name="heart-outline" size={size} color={color} />;
+          },
+        }}
+      />
     </tab.Navigator>
   );
 }
