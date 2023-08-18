c931b68553674b5ab4430780f212e600455f50bd
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-18 13:05:48 -0300

    Favoritar receita

---
+++ src/utils/storage.js
@@ -0,0 +1,44 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function getFavorites(key) {
+  const favorites = await AsyncStorage.getItem(key);
+
+  return favorites ? JSON.parse(favorites) : [];
+}
+
+export async function saveFavorite(key, newItem) {
+  let myFavorites = await getFavorites(key);
+
+  const hasItem = myFavorites.some((item) => item.id === newItem.id);
+
+  if (hasItem) {
+    console.log("ESSE ITEM JÁ EXISTE NA LISTA DE FAVORITOS");
+    return;
+  }
+
+  myFavorites.push(newItem);
+
+  await AsyncStorage.setItem(key, JSON.stringify(myFavorites));
+}
+
+export async function removeItem(id) {
+  let receipes = await getFavorites("@appreceitas");
+
+  let myFavorites = receipes.filter((item) => item.id !== id);
+
+  await AsyncStorage.setItem("@appreceitas", JSON.stringify(myFavorites));
+
+  return myFavorites;
+}
+
+export async function isFavorite(receipe) {
+  let myReceipes = await getFavorites("@appreceitas");
+
+  const favorite = myReceipes.find((item) => item.id === receipe.id);
+
+  if (favorite) {
+    return true;
+  }
+
+  return false; 
+}
