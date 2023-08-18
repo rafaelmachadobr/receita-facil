b808a4af26078d44c95df237f986aac1833a3c69
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-18 13:57:59 -0300

    Add animações

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -16,6 +16,8 @@ import api from "../../services/api";
 import { FoodList } from "../../components/foodlist";
 import { useNavigation } from "@react-navigation/native";
 
+import { Text as MotiText } from "moti";
+
 export function Home() {
   const [inputValue, setInputValue] = useState("");
   const [foods, setFoods] = useState([]);
@@ -42,8 +44,22 @@ export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Logo />
-      <Text style={styles.title}>Encontre a receita</Text>
-      <Text style={styles.title}>que combina com você</Text>
+      <MotiText
+        style={styles.title}
+        from={{ opacity: 0, translateY: 15 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ delay: 100, type: "timing", duration: 650 }}
+      >
+        Encontre a receita
+      </MotiText>
+      <MotiText
+        from={{ opacity: 0, translateY: 18 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ delay: 200, type: "timing", duration: 850 }}
+        style={styles.title}
+      >
+        que combina com você
+      </MotiText>
 
       <View style={styles.form}>
         <TextInput
